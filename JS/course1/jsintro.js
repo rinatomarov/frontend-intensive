b9d155363d11c321e303b7ef00e908bc8463dab5
rinatomarov <rok050600@bk.ru>
2023-11-16 19:52:25 +0600

    added js

---
+++ JS/course1/jsintro.js
@@ -0,0 +1,11 @@
+const hello = '3131' ;/*
+alert(hello)
+alert('You are noy unauthorized')
+
+confirm('Are you sure?')*/
+
+// prompt("assc")
+
+// alert(prompt("your name ???"))
+
+console.log(hello)
---
+++ JS/course1/task1.js
@@ -0,0 +1,8 @@
+alert('Вы не имеете сюда доступа')
+console.log("User authorized")
+let login = "Rinat"
+console.log(login)
+login = "Skye_Smit"
+console.log(login)
+const message = 'message'
+console.log(message)
