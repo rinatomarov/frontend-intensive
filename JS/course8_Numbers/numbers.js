1754d9739a2778c4831b3948241ca491b7c1b4f6
rinatomarov <rok050600@bk.ru>
2023-12-16 14:25:06 +0600

    js numbers

---
+++ JS/course8_Numbers/numbers.js
@@ -0,0 +1,21 @@
+const someNumber = 12.122312
+const someNumber2 = 12345.32323
+// console.log(someNumber.toFixed(2)) // - возвращает числовую строку
+// console.log(Number(someNumber.toFixed(2))) // приведение к числу
+// console.log(+someNumber.toFixed(2)) // приведение к числу
+//
+// console.log(16 + +someNumber.toFixed(2))
+//
+// console.log(someNumber.toString().length)
+//
+// console.log(Math.max(someNumber,someNumber2 ))
+// console.log(Math.min(someNumber,someNumber2 ))
+// console.log(Math.sqrt(4))
+
+// console.log(Math.floor(someNumber)) // округление к полу
+// console.log(Math.floor(someNumber2)) // округление к полу
+
+// console.log(Math.ceil(someNumber)) // округление к потолку
+// console.log(Math.ceil(someNumber2)) // округление к потолку
+
+console.log(Math.pow(2,5))
---
+++ JS/course8_Numbers/task_numbers.js
@@ -0,0 +1,29 @@
+// 1
+const number33 = '12'
+// console.log(typeof number33)
+// console.log(+number33, typeof +number33)
+// console.log(Number(number33) , typeof Number(number33))
+//
+// const num2 = 12.11
+// console.log( num2.toFixed(0) , typeof num2.toFixed(0))
+// console.log(num2.toString())
+
+// 2
+const num1 = 123.3435
+// console.log(+num1.toFixed(2))
+// console.log(Math.floor(num1))
+// console.log(Math.ceil(num1))
+
+// 3
+// const randomNumber = Math.round(Math.random() * 10)
+// console.log(randomNumber)
+// if (randomNumber > 5){
+//     console.log("Орел")
+// }else console.log("Решка")
+//
+// // 4
+// for (let idx = 0;idx<=100;idx++){
+//     if (Math.sqrt(idx).toString().length <=2){
+//         console.log(idx)
+//     }
+// }
