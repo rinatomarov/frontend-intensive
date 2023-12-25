56d88a0b850e668b19e80ef28bb38f35680e87c9
rinatomarov <rok050600@bk.ru>
2023-12-25 23:16:00 +0600

    js arrays

---
+++ JS/course11_arrays/index.js
@@ -0,0 +1,6 @@
+// 1 - callback
+const menu = [{food_name: "Pelmen", price: 4500},
+    {food_name: "Kasha", price: 1500},
+    {food_name: "Kazy-Karta", price: 9900}]
+const user = { user_name: "Dauke", has_promoCode: true}
+
