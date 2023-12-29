7721fd368111e3360cbf16eef74800e3bbd9502f
rinatomarov <rok050600@bk.ru>
2023-12-29 23:18:25 +0600

    добавил дз по array

--- JS/course11_arrays/index.js
+++ JS/course11_arrays/index.js
@@ -1,6 +1,32 @@
-// 1 - callback
+// 1.Если у клиента есть промокод, то показавать ему список блюд с 10% скидкой
 const menu = [{food_name: "Pelmen", price: 4500},
     {food_name: "Kasha", price: 1500},
     {food_name: "Kazy-Karta", price: 9900}]
-const user = { user_name: "Dauke", has_promoCode: true}
+const user =
+    {user_name: "Dauke", has_promoCode: true}
 
+if (user.has_promoCode) {
+    const discountedMenu = menu.map(element => {
+        return [element.food_name, element.price * 0.9]
+    })
+    // console.log("Цена со скидкой: " , discountedMenu)
+}
+
+// 2.Если у клиента нет промокода, то показавать ему список блюд без скидки
+else {
+    console.log("Цена без скидки: ", menu)
+}
+// 3.Если в меню есть хотя бы одно блюдо со стоимостью больше 5000тг, то перевернуть массив наоборот
+if (menu.some(element => element.price > 5000)) {
+    menu.reverse()
+    // console.log(menu)
+}
+
+// 4.Посчитать сколько в меню блюд, названия которых начинается на "К"
+console.log(menu.filter((element) => {
+    let count = 0
+    if (element.food_name.at(0)==='K'){
+        count =count+1
+        return count
+    }
+}))
---
+++ JS/course11_arrays/newindex.js
@@ -0,0 +1,54 @@
+const array = [1,2,3,4,5,6,7,8,9]
+
+// console.log(array.reverse())
+// console.log(array)
+
+// ''.includes()
+
+// console.log(array.includes(13))
+
+// console.log(array.indexOf(3))
+
+// console.log(array.lastIndexOf(3))
+
+// console.log(array.sort())
+// console.log(array)
+
+// console.log(array.slice(2, 5))
+// console.log(array.slice(3))
+// console.log(array)
+
+// console.log(array.splice(2, 3, 'hello'))
+// console.log(array)
+
+// function callback(element, index) {
+//     console.log(element, index)
+// }
+//
+// array.forEach((element, index) => {
+//     console.log(element, index)
+//
+//     element = element * 2
+// })
+
+// console.log(
+//     array.map((element, index) => {
+//         return element * 2
+//     })
+// )
+
+// console.log(array.some((element, index) => {
+//     return element % 67 === 0
+// }))
+
+// console.log(array.every((element, index) => {
+//     return element > 1
+// }))
+
+// console.log(array)
+
+// console.log('#######')
+//
+// for(let i = 0; i < array.length; i++) {
+//     callback(array[i], i)
+// }
