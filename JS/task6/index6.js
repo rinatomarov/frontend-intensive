a343580820ab86f44e95c753297156eccd1f584c
rinatomarov <rok050600@bk.ru>
2024-01-17 21:20:15 +0600

    js task6

--- JS/task6/index6.js
+++ JS/task6/index6.js
@@ -1,5 +1,16 @@
-const input = document.querySelectorAll('input');
+const input = document.querySelector('input');
 
 input.addEventListener('keydown', (event) => {
-    console.log(input.value)
+    if (!Number.isInteger(Number(event.key))) {
+        alert("Че то не то!")
+    }
 })
+
+//
+// input.addEventListener('keydown', (event) => {
+//     const numbers = [0,1,2,3,4,5,6,7,8,9]
+//
+//     if(!numbers.some(num => event.key === num.toString())) {
+//         alert('Можно вводить только числа')
+//     }
+// })
