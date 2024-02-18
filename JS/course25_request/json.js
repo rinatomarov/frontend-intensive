798ffa7a8845568f4a2559c5234ab295422fb8e4
rinatomarov <rok050600@mail.ru>
2024-02-18 17:49:51 +0600

    задача про получить посты и показать их на странице

--- JS/course25_request/json.js
+++ JS/course25_request/json.js
@@ -1,14 +1,16 @@
-async function loadTodo () {
+async function loadPost () {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts')
     // console.log(await response.json())
     return  response.json()
 }
 
-const arrayForMe = loadTodo()
-console.log(arrayForMe)
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => {
+        return response.json()
+    })
+    // .then(posts =>console.log(posts))
+    // .then(json => console.log(json))
 
-const post = document.createElement('div')
-post.classList.add('post')
 
 // 2
 const parent = document.querySelector('.parent')
@@ -32,15 +34,55 @@ btn.addEventListener('click' , () => {
     fetch('https://jsonplaceholder.typicode.com/posts', {
         method: 'POST',
         body: JSON.stringify({
-            title: name,
-            body: textP,
+            title: namePost,
+            body: textPost,
             userId: 1,
-        })
+        }),headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
     }).then(response => {
         return response.json()
-    }).then(json => console.log(json))
-    parent.appendChild(post)
+    }).then(json => console.log(json)).then(parent.appendChild(post))
 
 
+
+    const btnDelete = document.createElement('button')
+    btnDelete.innerText = 'удалить'
+    post.appendChild(btnDelete)
+
+    const btnUpdate = document.createElement('button')
+    btnUpdate.innerText = 'редактировать'
+    post.appendChild(btnUpdate)
+
+    btnDelete.addEventListener('click' , () => {
+        // alert("delete")
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'DELETE'
+        })
+            .then(response => {
+                return response.json()
+            })
+            .then(json => console.log(json))
+
+        post.remove(post)
+    })
+
+    btnUpdate.addEventListener('click' , () => {
+        // alert("update")
+        fetch('https://jsonplaceholder.typicode.com/posts/1', {
+            method: 'PUT',
+            // сделать так чтобы  он менял
+            body: JSON.stringify({
+                title: prompt("Прошу ввести тему "),
+                body: prompt("Прошу ввести текст поста "),
+                userId: 1,
+            }),headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        }).then(response => {
+            return response.json()
+        }).then(json => console.log(json))
+
+    })
 })
 
