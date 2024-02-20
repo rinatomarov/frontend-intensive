e2d099b2c3744437df24e84a2983835739ddf30f
rinatomarov <rok050600@mail.ru>
2024-02-21 00:41:02 +0600

    task axios

--- JS/course26_js_axios/axios.js
+++ JS/course26_js_axios/axios.js
@@ -1,8 +1,13 @@
-    // axios.get('http://localhost:3000/posts')
-    //     .then(response => {
-    //         console.log(response.data)
-    //     })
+//1 - получение всех записей в посте бд
+// axios.get('http://localhost:3000/posts/')
+//         .then(response => {
+//             lenObj = response.data.length
+//             // console.log(lenObj)
+//             console.log(response.data[0].title)
+//             return lenObj
+//         })
 
+//2- добавление  записей в пост бд
 // axios.post('http://localhost:3000/posts', {
 //     id: 3,
 //     title: 'title skye',
@@ -12,45 +17,48 @@
 //     id: 4,
 //     title: 'title dauren',
 // })
-// axios.post('http://localhost:3000/posts', {
-//     id: 5,
-//     title: 'title friend',
-// })
-// axios.post('http://localhost:3000/posts', {
-//     id: 6,
-//     title: 'title gera',
-// })
-// axios.post('http://localhost:3000/posts', {
-//     id: 7,
-//     title: 'title rincho',
-// })
-// axios.post('http://localhost:3000/posts', {
-//     id: 8,
-//     title: 'title alizh',
-// })
 
-let id  = 1
+
+//3- создание  записей в пост (в файле html)
 const parent = document.querySelector('ul')
-const liTitles = document.createElement('li')
 
-const postJson = axios.get(`http://localhost:3000/posts/${id}`)
-    .then(response => {
-        console.log(response.data.title)
-        liTitles.innerText = response.data.title
-        console.log(liTitles)
+// for (let id= 1;id<12;id++){
+//     axios.get(`http://localhost:3000/posts/`)
+//         .then(response => {
+//             // console.log(response.data[12].title)
+//             const liTitles = document.createElement('li')
+//             liTitles.innerText = response.data[id].title
+//             // console.log(liTitles)
+//             parent.appendChild(liTitles)
+//         })
+// }
+
+axios.get(`http://localhost:3000/posts/`).then(response => {
+    for (let id = 1; id < response.data.length; id++) {
+        const liTitles = document.createElement('li')
+        liTitles.innerText = response.data[id].title
         parent.appendChild(liTitles)
+    }
+})
+
+
+//4 - получение значение в форме инпут и поиск из элементов li
+
+const btn = document.querySelector('button')
+btn.addEventListener('click', () => {
+    let dataInput = document.querySelector('input').value
+    console.log(dataInput)
+    parent.innerHTML = '';
+    axios.get(`http://localhost:3000/posts/`).then(response => {
+        for (let id = 1; id < response.data.length; id++) {
+            const liTitles = document.createElement('li')
+            if (response.data[id].title.toLowerCase().includes(dataInput.toLowerCase())) {
+                liTitles.innerText = response.data[id].title
+                parent.appendChild(liTitles)
+            }
+        }
     })
+})
 
 
-
-//
-for (let id= 1;id<6;id++){
-    axios.get(`http://localhost:3000/posts/`)
-        .then(response => {
-            console.log(response.data.title)
-            liTitles.innerText = response.data.title
-            console.log(liTitles)
-            parent.appendChild(liTitles)
-        })
-}
 
